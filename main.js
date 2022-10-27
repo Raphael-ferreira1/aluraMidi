82467ac37ef1143f39ed4019033b3459d6690f22
Raphael-ferreira1 <111326322+Raphael-ferreira1@users.noreply.github.com>
2022-10-27 17:58:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento === null) {
+        
+    }
+
+    if (elemento != null && elemento.localName === 'audio'){
+
+            elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento  não encontrado ou seletor invalido');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+let contador = 0
+
+//enquanto
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;//template String
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    console.log(contador);
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function () {
+            tecla.classList.remove('ativa');
+    }
+
+    }
+
+}
